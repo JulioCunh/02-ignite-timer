dc88c10aa3cb9364984e4f665b5c24403da780ef
JulioCunh <fcezarf@gmail.com>
2023-01-02 07:23:31 -0300

    Header & Layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { Router } from './Router'
 
-import { GlobalStyle } from './src/styles/global'
-import { defaultTheme } from './src/styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
