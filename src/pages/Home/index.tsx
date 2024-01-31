8a3793c8c6ddf7e79f8b256710fd4b1052b94cc3
JulioCunh <fcezarf@gmail.com>
2024-01-31 19:57:12 -0300

    Finalização da Home 1

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,37 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StarCountdownButton,
+} from './styles'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">Vou trabalhar em</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>00</span>
+          <Separator>:</Separator>
+          <span>00</span>
+        </CountdownContainer>
+
+        <StarCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StarCountdownButton>
+      </form>
+    </HomeContainer>
+  )
 }
