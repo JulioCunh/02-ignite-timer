590d021cd04f89dff81695c18fd1691e780db0d5
Julio Cunha <fcezarf@gmail.com>
2024-10-27 08:51:55 -0300

    Contexto no formulário

--- src/pages/Home/components/NewCycleForm/styles.ts
+++ src/pages/Home/components/NewCycleForm/styles.ts
@@ -31,7 +31,7 @@ const BaseInput = styled.input`
   }
 `
 
-export const TaskInpult = styled(BaseInput)`
+export const TaskInput = styled(BaseInput)`
   flex: 1;
   &::-webkit-calendar-picker-indicator {
     display: none !important;
